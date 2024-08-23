3b238871f471698587b6e15be57e23f0a46d17b9
LuthMC <luthfi366822@gmail.com>
2024-08-23 12:56:16 +0700

    Update EventListener.php

--- src/Luthfi/LoginUI/EventListener.php
+++ src/Luthfi/LoginUI/EventListener.php
@@ -1,29 +1,106 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Luthfi\LoginUI;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\player\Player;
+use Luthfi\LoginUI\libs\LootSpace369\LSFormAPI\SimpleForm;
+use Luthfi\LoginUI\libs\LootSpace369\LSFormAPI\CustomForm;
+use Luthfi\LoginUI\libs\LootSpace369\LSFormAPI\ModalForm;
 
-class EventListener implements Listener
-{
-    /** @var Main */
+class EventListener implements Listener {
+
     private $plugin;
+    private $loginAttempts = [];
+    private $playerDataManager;
 
-    public function __construct(Main $plugin)
-    {
+    public function __construct(Main $plugin) {
         $this->plugin = $plugin;
+        $this->playerDataManager = new PlayerDataManager($plugin);
     }
 
-    public function onPlayerJoin(PlayerJoinEvent $event): void
-    {
+    public function onPlayerJoin(PlayerJoinEvent $event): void {
         $player = $event->getPlayer();
-        if ($this->plugin->getPlayerData()->exists($player->getName())) {
-            $this->plugin->showLoginForm($player);
+        if (!$player->hasPlayedBefore()) {
+            $this->sendRegisterForm($player);
         } else {
-            $this->plugin->showRegisterOrLoginForm($player);
+            $this->sendLoginForm($player);
         }
+    }
+
+    private function sendRegisterForm(Player $player): void {
+        $form = new CustomForm(function (Player $player, ?array $data) {
+            if ($data === null) return;
+
+            $username = $data[0];
+            $password = $data[1];
+            $pin = $this->plugin->getConfig()->get("enable-pin") ? $data[2] : null;
+
+            $this->playerDataManager->registerPlayer($player, $password, $pin);
+
+            $player->sendMessage($this->plugin->getConfig()->get("messages")["register-success"]);
+
+            if ($pin !== null) {
+                $this->sendPinConfirmationForm($player);
+            }
+        });
+
+        $form->setTitle("Register");
+        $form->addInput("Username:");
+        $form->addInput("Password:");
+
+        if ($this->plugin->getConfig()->get("enable-pin")) {
+            $form->addInput("PIN:");
+        }
+
+        $player->sendForm($form);
+    }
+
+    private function sendLoginForm(Player $player): void {
+        if (!isset($this->loginAttempts[$player->getName()])) {
+            $this->loginAttempts[$player->getName()] = 0;
+        }
+
+        $form = new CustomForm(function (Player $player, ?array $data) {
+            if ($data === null) return;
+
+            $username = $data[0];
+            $password = $data[1];
+
+            if ($this->playerDataManager->validateLogin($player, $password)) {
+                $player->sendMessage($this->plugin->getConfig()->get("messages")["login-success"]);
+                unset($this->loginAttempts[$player->getName()]);
+            } else {
+                $this->loginAttempts[$player->getName()]++;
+                if ($this->loginAttempts[$player->getName()] >= $this->plugin->getConfig()->get("max-login-attempts")) {
+                    $player->sendMessage($this->plugin->getConfig()->get("messages")["max-attempts"]);
+                    $player->kick($this->plugin->getConfig()->get("messages")["max-attempts"], false);
+                } else {
+                    $player->sendMessage($this->plugin->getConfig()->get("messages")["login-fail"]);
+                    $this->sendLoginForm($player);
+                }
+            }
+        });
+
+        $form->setTitle("Login");
+        $form->addInput("Username:");
+        $form->addInput("Password:");
+
+        $player->sendForm($form);
+    }
+
+    private function sendPinConfirmationForm(Player $player): void {
+        $form = new ModalForm("Confirmation", "Are you sure you want to proceed?", "Yes", "No", function (Player $player, ?bool $data) {
+            if ($data === null) return;
+
+            if ($data) {
+                $player->sendMessage("Yes!");
+            } else {
+                $player->sendMessage("No!");
+            }
+        });
+
+        $player->sendForm($form);
     }
 }
